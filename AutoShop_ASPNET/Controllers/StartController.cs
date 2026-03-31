88b22fb961d12a951ba02d51a2ab116dd67ee34e
DaniilEvseev <Danchic2033@yandex.ru>
2026-03-31 19:47:24 +0500

    тестовое задание 1

---
+++ AutoShop_ASPNET/Controllers/StartController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AutoShop_ASPNET.Controllers
+{
+    public class StartController : Controller
+    {
+        public string Hello()
+        {
+            DateTime dateTime = DateTime.Now;
+
+            var result = String.Empty;
+
+            if (dateTime.Hour < 6)
+            {
+                result = "Доброй ночи";
+            }
+            else if (dateTime.Hour < 12)
+            {
+                result = "Доброе утро";
+            }
+            else if (dateTime.Hour < 18)
+            {
+                result = "Добрый день";
+            }
+            else
+            {
+                result = "Добрый вечер";
+            }
+            return result;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- AutoShop_ASPNET/Program.cs
+++ AutoShop_ASPNET/Program.cs
@@ -17,7 +17,7 @@ namespace AutoShop_ASPNET
             app.MapStaticAssets();
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}")
+                pattern: "{controller=Start}/{action=Hello}/{id?}")
                 .WithStaticAssets();
 
             app.Run();
