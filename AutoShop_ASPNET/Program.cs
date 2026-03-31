ecab3e6cd5e981be72ba27ddcec6846ebcfe3141
DaniilEvseev <Danchic2033@yandex.ru>
2026-03-31 14:57:51 +0500

    Add project files.

---
+++ AutoShop_ASPNET/Program.cs
@@ -0,0 +1,26 @@
+namespace AutoShop_ASPNET
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.MapStaticAssets();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}")
+                .WithStaticAssets();
+
+            app.Run();
+        }
+    }
+}
