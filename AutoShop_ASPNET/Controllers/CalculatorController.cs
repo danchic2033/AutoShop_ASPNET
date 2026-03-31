9c703f5e9e3a77b659b84cd77b3138e068bc1c94
DaniilEvseev <Danchic2033@yandex.ru>
2026-03-31 20:03:21 +0500

    Сделал задачу 3

--- AutoShop_ASPNET/Controllers/CalculatorController.cs
+++ AutoShop_ASPNET/Controllers/CalculatorController.cs
@@ -4,9 +4,21 @@ namespace AutoShop_ASPNET.Controllers
 {
     public class CalculatorController : Controller
     {
-        public int Index(int num1, int num2)
+        public string Index(int num1, int num2, string symbol)
         {
-            return num1 + num2;
+            if (symbol != "+" && symbol != "*" && symbol != null)
+            {
+                return "Введите символ + или * или оставьте последний сегмент пустым";
+            }
+            switch (symbol) 
+            {
+                case "+":
+                    return (num1 + num2).ToString();
+                case "*":
+                    return (num1 * num2).ToString();
+                default:
+                    return (num1 + num2).ToString();
+            }
         }
     }
 }
--- AutoShop_ASPNET/Program.cs
+++ AutoShop_ASPNET/Program.cs
@@ -17,7 +17,7 @@ namespace AutoShop_ASPNET
             app.MapStaticAssets();
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Calculator}/{action=index}/{num1?}/{num2?}")
+                pattern: "{controller=Calculator}/{action=index}/{num1?}/{num2?}/{symbol?}")
                 .WithStaticAssets();
 
             app.Run();
