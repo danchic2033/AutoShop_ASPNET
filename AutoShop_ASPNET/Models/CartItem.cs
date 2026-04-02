607aed203db446471c1df5ff3b136154abeb44da
DaniilEvseev <Danchic2033@yandex.ru>
2026-04-02 12:09:57 +0500

    Создал модель корзины и модель позиции из корзины

---
+++ AutoShop_ASPNET/Models/CartItem.cs
@@ -0,0 +1,9 @@
+﻿namespace AutoShop_ASPNET.Models
+{
+    public class CartItem
+    {
+        public Product Product { get; set; }
+        public int ItemQuantity { get; set; }
+        public decimal ItemCost { get; set; }
+    }
+}
