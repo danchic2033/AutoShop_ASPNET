130850d7bb1a93a2dc74da84f2ddc8989dcd313e
DaniilEvseev <Danchic2033@yandex.ru>
2026-04-03 14:17:24 +0500

    Рефакторинг моделей Cart и CartItem

--- AutoShop_ASPNET/Models/Cart.cs
+++ AutoShop_ASPNET/Models/Cart.cs
@@ -2,7 +2,10 @@
 {
     public class Cart
     {
+        public Guid Id { get; set; }
+        public string UserId { get; set; }
         public List<CartItem> CartItems { get; set; }
+        public decimal TotalCost => CartItems.Sum(item => item.ItemCost);
 
         public Cart (List<CartItem> cartItems)
         {
