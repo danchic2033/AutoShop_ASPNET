4ec37705e81ec28b29191d4099738c975e4a8f14
DaniilEvseev <Danchic2033@yandex.ru>
2026-04-06 07:39:17 +0500

    Реализовал страницу оформления заказа пользователем

---
+++ AutoShop_ASPNET/Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using AutoShop_ASPNET.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoShop_ASPNET.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly ICartRepository _cartRepository;
+        public OrderController (ICartRepository cartRepository)
+        {
+            _cartRepository = cartRepository;
+        }
+        
+        public IActionResult SuccessOrder()
+        {
+            _cartRepository.RemoveCart();
+            return View("SuccessOrder");
+        }
+
+        public IActionResult Index()
+        {
+            var cart = _cartRepository.GetCart();
+            return View(cart);
+        }
+    }
+}
---
+++ AutoShop_ASPNET/Models/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace AutoShop_ASPNET.Models
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public string UserId { get; set; }
+        public List<CartItem> CartItems { get; set; }
+    }
+}
