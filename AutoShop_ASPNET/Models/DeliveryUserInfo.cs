af251bd3f6809eeae77f5231692b7ef49fcd35fb
DaniilEvseev <Danchic2033@yandex.ru>
2026-04-16 07:30:42 +0500

    Создал модель описывающую информацию о доставке

---
+++ AutoShop_ASPNET/Models/DeliveryUserInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace AutoShop_ASPNET.Models
+{
+    public class DeliveryUserInfo
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string DateDelivery { get; set; }
+        public string? Comment { get; set; }
+    }
+}
