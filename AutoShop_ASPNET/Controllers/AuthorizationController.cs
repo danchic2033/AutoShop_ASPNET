8e485be5ce166f36128a673f693ac8c1a7b4f287
DaniilEvseev <Danchic2033@yandex.ru>
2026-04-19 07:27:05 +0500

    сделал форму авторизации. она отображается

---
+++ AutoShop_ASPNET/Controllers/AuthorizationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AutoShop_ASPNET.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
