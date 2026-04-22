69c20aadb2d3d469e8b5fe5ebe48bde180a4079c
DaniilEvseev <Danchic2033@yandex.ru>
2026-04-22 07:40:48 +0500

    Создал контроллер для отображения формы регистрации. Сделал представление для регистрации

---
+++ AutoShop_ASPNET/Controllers/RegistrationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AutoShop_ASPNET.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
