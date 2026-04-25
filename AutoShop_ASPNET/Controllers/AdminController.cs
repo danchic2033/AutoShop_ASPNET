3c72b3eed12255eecf9ae238f501747638cbd25c
DaniilEvseev <Danchic2033@yandex.ru>
2026-04-25 16:01:34 +0500

    Добавил кнопку панель администратора в шапку

---
+++ AutoShop_ASPNET/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AutoShop_ASPNET.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
