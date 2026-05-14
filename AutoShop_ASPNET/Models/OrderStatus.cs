1074de805576836c4e8e3c0227e6b116dbcdde10
DaniilEvseev <Danchic2033@yandex.ru>
2026-05-14 07:42:32 +0500

    добавил представление для отображение детальной информации о заказе + добавил enum статуса заказов

---
+++ AutoShop_ASPNET/Models/OrderStatus.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoShop_ASPNET.Models
+{
+    public enum OrderStatus
+    {
+        [Display(Name = "Создан")]
+        Created,
+
+        [Display(Name = "Обработка")]
+        Processed,
+
+        [Display(Name = "В пути")]
+        Delivering,
+
+        [Display(Name = "Доставлен")]
+        Delivered,
+
+        [Display(Name = "Отменен")]
+        Canceled
+    }
+}
