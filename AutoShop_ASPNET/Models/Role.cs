f746f06caf4d22fed28c20e53a89364bb70109e3
DaniilEvseev <Danchic2033@yandex.ru>
2026-05-17 11:11:02 +0500

    Валидировал модель Role

---
+++ AutoShop_ASPNET/Models/Role.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoShop_ASPNET.Models
+{
+    public class Role
+    {
+        [Display(Name = "Имя", Prompt = "Введите название роли")]
+        [Required(ErrorMessage = "Необходимо ввести название роли")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Длина имени должна быть от {2} до {1} символов")]
+        public string Name { get; set; }
+    }
+}
